f3eb9aa01233aa1d16ff3a59ef01eebadca1fa7e
yossi <a534158711@gmail.com>
2023-08-01 12:00:05 +0300

    quicksort sort

---
+++ src/10-Algorithms/04-sorts/quicksort.ts
@@ -0,0 +1,41 @@
+var cycles = 0;
+
+function quickSort(arr){
+    if( arr.length <= 1 ){ return arr; }
+    let tmp = arr[0]
+    if( arr.length == 2 ){
+        if( arr[0] > arr[1] ){ 
+            arr[0] = arr[1];
+            arr[1] = tmp; 
+        }
+        return arr;
+    }
+    let cycle = ++cycles;
+    let pivot = Math.floor(arr.length * Math.random());
+    let pivotValue = arr[pivot];
+    let arrLeft = [];
+    let arrRight = [];
+    let arrPivot = [];
+    for( let i=0; i<arr.length; i++ ){
+        if( pivotValue === arr[i] ){
+            arrPivot.push(pivotValue);
+        }
+        else if( pivotValue > arr[i] ){
+            arrLeft.push(arr[i]);
+        }
+        else{
+            arrRight.push(arr[i]);
+        }
+    }
+    console.log('|\tCycle #'+cycle+' Start\tarrLeft='+arrLeft+'\tarrPivot='+arrPivot+'\tarrRight='+arrRight);
+    arrLeft = quickSort(arrLeft);
+    arrRight = quickSort(arrRight);
+    let arrSorted = [].concat(arrLeft, arrPivot, arrRight);
+    console.log('|\tCycle #'+cycle+' End\tarrLeft='+arrLeft+'\tarrPivot='+arrPivot+'\tarrRight='+arrRight);
+    return arrSorted;
+}
+
+export {
+    quickSort
+    
+}
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
-import { initializeInputArray, sortByBSTInsert } from "./10-Algorithms/04-sorts/insert";
+import { quickSort } from "./10-Algorithms/04-sorts/quicksort";
 
-var arr1 = initializeInputArray(10);
-var out1 = sortByBSTInsert(arr1);
-console.log("Inoput = "+arr1.toString());
-console.log("Output = "+out1.toString());
+console.info('+----------------------------------------');
+console.info('| Quick Sort - create values array:');
+var cycles = 0;
+var arrValues2 = [ -1, 0 , -1, 1, 1, 1, -1, 0, 0, 1, -1, 1, 0 ];
+console.log('|\tarrValues2 =\t'+arrValues2.toString());
+var arrOut2 = quickSort(arrValues2);
+console.log('|\tarrOut2 =\t'+arrOut2.toString());
